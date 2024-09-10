0858dcc44e0707579d670570c75862e13285d041
Joey chessor <jchesso2@ut.utm.edu>
2024-09-10 18:17:50 -0500

    test me g++ not working

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 
-#include <iostream>
+ #include <iostream>
+ #include <iomanip>
 
-using namespace std;
+ using namespace std;
+ void sum(int var2, int var1){
+  int add = var2 + var1;
+  double ave = add / 2;
+  cout<<ave;
+ }
 
-int main() {
-  cout<<"Hello World!"<<endl;
-  
-  return 0;
+ int main() {
+  int var2 = 0;
+  int var1 = 0;
+  cout<<"enter the numbers to be averaged"<<endl;
+  cin>>var1;
+  cout<<endl<<"enter the second number";
+  cin>>var2;
+  cout<<endl;
+  sum(var2, var1);
 }
